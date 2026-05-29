04ad6c652588fe836fda1edb6d6cfc3b67b473d9
Angixs-zz <hpma.305@gmail.com>
2026-05-28 20:56:35 -0600

    chore: align build.sh content exactly with checklist

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Instalar dependencias
 pip install -r requirements.txt
-
-# Ejecutar migraciones de la base de datos
 python manage.py migrate
-
-# Recolectar archivos estáticos
 python manage.py collectstatic --no-input
