98aba7d118806ec1e80e4e49a7801ae81fd26cf3
Angixs-zz <hpma.305@gmail.com>
2026-06-03 12:23:24 -0600

    Se arreglo parte del api

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 # config/urls.py
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from usuarios import views as usuarios_views
 
 urlpatterns = [
@@ -28,5 +29,8 @@ urlpatterns = [
     path('reportes/', include('reportes.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
